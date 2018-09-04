990237d2377d14ef0866f850b2f5f4c8b96a4770
anuradha prasanna <anuprasanna@mitrai.com>
2018-09-04 13:37:34 +0530

    enhance the job1 project

--- job1-spring-boot-api-sample/main.groovy
+++ job1-spring-boot-api-sample/main.groovy
@@ -1,20 +1,26 @@
 String basePath = 'springboot-api-demo'
 String repo = 'anuprasanna/springboot-api-demo'
+String buildBranch = '*/master'
 
 folder(basePath) {
-    displayName('Springboot API Demo')
+    displayName('Springboot API Sample')
     description 'This example shows basic folder/job creation.'
 }
 
-
+// job definition
 job(basePath + '/Build') {
     scm {
-      github repo  
-    }
-    triggers {
-        scm('H/15 * * * *')
+      github (repo, buildBranch, 'https', 'github.com')
     }
     steps {
-        maven('-e clean install')
+      	maven {
+            goals('clean')
+            goals('install')
+            mavenOpts('-Xms256m')
+            mavenOpts('-Xmx512m')
+            properties(skipTests: true)
+            mavenInstallation('Maven3')
+            providedSettings('central-mirror')
+        }
     }
 }
